e5100e7ff99fc9e24ff412dbd730d29411b9729a
pintr <pintr@DESKTOP-I0IEGU3.attlocal.net>
2023-04-11 19:04:46 -0400

    Intial Commit

---
+++ src/Rudy/Test.java
@@ -0,0 +1,15 @@
+package Rudy;
+
+public class Test {
+	public static void main(String[] args) {
+		
+	}
+	public void  add1(int a, int b) {
+		System.out.println(a + b);
+	}
+	
+	public void add2(int a, int b) {
+		System.out.println(a + b);
+	}
+
+}
