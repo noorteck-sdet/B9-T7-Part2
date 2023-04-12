b37bb33a031c7da50bd177793e3e349f6b5cce85
Belmin <Belmin@DESKTOP-E8ECCRD>
2023-04-12 16:03:27 -0400

    Belmin Initial

---
+++ src/Belmin/BelminClass1.java
@@ -0,0 +1,20 @@
+package Belmin;
+
+public class BelminClass1 {
+	
+	public static void main(String[] args) {
+		
+	}
+	public void print() {
+		
+		System.out.println("TEST");
+		
+	}
+	
+	public void add() {
+		
+		System.out.println(9+9);
+		
+	}
+
+}
