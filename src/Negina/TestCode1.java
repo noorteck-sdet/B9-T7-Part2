08dd92e28c3140fe23f7ecf2b57fa9fed86df1be
hedas <hedas@10.0.0.133>
2023-04-12 17:36:33 -0700

    first class

---
+++ src/Negina/TestCode1.java
@@ -0,0 +1,13 @@
+package Negina;
+
+public class TestCode1 {
+public static void main(String[] args) {
+	
+	
+	 System.out.println("Hello everyone");
+}
+
+public void message() {
+	System.out.println("This is my firt method");
+}
+}
